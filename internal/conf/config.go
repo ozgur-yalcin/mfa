5967e52e0515b32c18de772d8d0ec9c66b1b1e44
Özgür Yalçın <ozguryalcin@outlook.com>
2024-12-01 21:54:38 +0300

    x

--- internal/conf/config.go
+++ internal/conf/config.go
@@ -1,12 +1,12 @@
 package conf
 
 import (
-	backend2 "github.com/ozgur-yalcin/mfa/internal/backend"
+	"github.com/ozgur-yalcin/mfa/internal/backend"
 	"github.com/ozgur-yalcin/mfa/internal/fs"
 )
 
 type Config struct {
-	DatabaseBackend backend2.Backend
+	DatabaseBackend backend.Backend
 }
 
 const (
@@ -16,5 +16,5 @@ const (
 func DefaultConfig() *Config {
 	path := fs.MakeFilenamePath(sqliteFileName)
 	//fmt.Println(path)
-	return &Config{DatabaseBackend: backend2.NewSqlite(path)}
+	return &Config{DatabaseBackend: backend.NewSqlite(path)}
 }
